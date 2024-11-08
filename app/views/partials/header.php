d751d79c044fe4acbe48fb48b62005d209010b36
mario4off <mario31991@gmail.com>
2024-11-08 16:39:28 +0100

    "subo el header.php completado"

---
+++ app/views/partials/header.php
@@ -0,0 +1,38 @@
+<header>
+    <div class="upper-banner-header container-fluid ">
+        <div class="row div-upper-header">
+            <div class="col-sd-12 col-md-6 col-ld-6  d-flex justify-content-center align-items-center">
+                <p>Regístrate Ahora: -10% de descuento en tu primer pedido</p>
+            </div>
+            <div class="col-12 col-md-6 col-ld-6 d-flex justify-content-center align-items-center d-none d-md-flex">
+                <p>Todos los Martes: TODAS las hamburguesas al mismo precio</p>
+            </div>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-lg custom-navbar">
+        <div class="container header-container">
+            <img id="header-logo" class="image-fluid" src="../../../public/images/dr_burger_logo.webp" alt="">
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div>
+                    <ul class="navbar-nav ">
+                        <li><a class=" active" aria-current="page" href="#">INICIO</a></li>
+                        <li><a class="nav-link header-link" href="#">MENÚ</a></li>
+                        <li><a class="nav-link header-link" href="#">CONTACTO</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <ul class="navbar-nav-right ms-auto d-flex header-icons">
+                    <li><a class="nav-link" href="#"><i class="fa-solid fa-user-large"></i></a></li>
+                    <li><a class="nav-link" href="#"><i class="fa-solid fa-bag-shopping"></i></a></li>
+                    <li><a class="nav-link" href="#"><i class="fa-solid fa-arrow-right-from-bracket"></i></a></li>
+                </ul>
+            </div>
+            <button id="header-toggle" class="navbar-toggler .d-none" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </nav>
+</header>
