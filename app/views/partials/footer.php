bfb4fa3cacf2ef71e5eae22bd9ef916d00f91f14
mario4off <mario31991@gmail.com>
2024-11-11 07:36:24 +0100

    "header y footer terminados"

---
+++ app/views/partials/footer.php
@@ -0,0 +1,109 @@
+<footer>
+    <section class="container-fluid newsletter-container">
+
+        <div class="container body-newsletter">
+            <h3>SUSCRÍBETE PARA RECIBIR INFO SOBRE EVENTOS Y OFERTAS</h3>
+            <p class="p-highlight">10% de descuento para nuevos registros</p>
+            <p class="p-white">Me gustaría recibir noticias sobre:</p>
+            <form class="form-newsletter mt-1 d-md-flex flex-dm-row" action="">
+                <div class="row checkbox-newsletter col-12 col-md-6">
+                    <div class="input-checkbox col-12 col-md-4 d-flex align-items-center">
+                        <div class="form-check">
+                            <input type="checkbox" id="burger" name="burger" value="burger">
+                            <label class="form-check-label" for="burger">Hamburguesas</label>
+                        </div>
+                    </div>
+                    <div class="input-checkbox  col-12 col-md-4 d-flex align-items-center">
+                        <div class="form-check">
+                            <input type="checkbox" id="complements" name="complements" value="complements">
+                            <label class="form-check-label" for="complements">Complementos</label>
+                        </div>
+                    </div>
+                    <div class="input-checkbox  col-12 col-md-4 d-flex align-items-center">
+                        <div class="form-check">
+                            <input type="checkbox" id="promos" name="promos" value="promos">
+                            <label class="form-check-label" for="promos">Promos</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row newsletter-form-mail col-12 col-md-6 mt-sm-3 ">
+                    <div class="col-12 col-md-8 input-mail">
+                        <input class="input-textbox w-99" type="email"
+                            placeholder="Introduce tu dirección de correo electrónico" required>
+                    </div>
+                    <div class="col-12 col-md-4 input-mail">
+                        <button type="submit" class="input-submit w-99">SUSCRIBIRME</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </section>
+    <section class="container-fluid general-links d-flex align-items-center justify-content-center ">
+        <div class="container w-75">
+            <div class="row container footer-row-links d-flex justify-content-between mt-5 mb-5">
+                <div class="col-md-2"><a class="main-footer-links" href="">INICIAR SESIÓN / REGISTRARSE</a></div>
+                <div class="col-md-2">
+                    <h4>CONÓCENOS</h4>
+                    <ul class="ps-0 ul-footer">
+                        <li><a class="link-footer" href="">HISTORIA</a></li>
+                        <li><a class="link-footer" href="">TRABAJA AQUÍ</a></li>
+                        <li><a class="link-footer" href="">RRSS</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <h4>INFORMACIÓN ÚTIL</h4>
+                    <ul class="ps-0 ul-footer">
+                        <li><a class="link-footer" href="">MI PEDIDO</a></li>
+                        <li><a class="link-footer" href="">FAQ</a></li>
+                        <li><a class="link-footer" href="">ENTREGAS</a></li>
+                        <li><a class="link-footer" href="">DEVOLUCIONES</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h4>INFORMACIÓN COPORATIVA Y LEGAL</h4>
+                    <ul class="ps-0 ul-footer">
+                        <li><a class="link-footer" href="">TÉRMINOS Y CONDICIONES</a></li>
+                        <li><a class="link-footer" href="">POLÍTICAS DE PRIVACIDAD</a></li>
+                        <li><a class="link-footer" href="">POLÍTICA DE COOKIES</a></li>
+                        <li><a class="link-footer" href="">CAMBIAR PREFERENCIAS</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <h4>COMPRAS SEGURAS EN LÍNEA</h4>
+                    <ul class="d-flex justify-content-around ps-0">
+                        <li><img class="payment-icons" src="../../../public/images/visa.webp" alt="visa">
+                        </li>
+                        <li><img class="payment-icons l-icon" src="../../../public/images/mastercard.webp"
+                                alt="mastercad">
+                        </li>
+                        <li><img class="payment-icons l-icon" src="../../../public/images/maestro.webp" alt="maestro">
+                        </li>
+                        <li><img class="payment-icons" src="../../../public/images/american-express.webp"
+                                alt="american express"></li>
+                        <li><img class="payment-icons" src="../../../public/images/paypal.webp" alt="paypal"></li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container-fluid container-rights">
+        <div class="row row-rights container">
+            <div class="col-12 col-md-4 col-ld-4 d-flex align-items-center justify-content-center">
+                <p class="p-rights pt-1">DESGINED BY MARIO POZO © 2024</p>
+            </div>
+            <div class="col-12 col-md-4 col-ld-4 d-flex align-items-cente justify-content-center">
+                <div class="social-media-links">
+                    <a href=""><i class="fa-brands fa-facebook"></i></a><a href=""><i
+                            class="fa-brands fa-x-twitter"></i></a><a href=""><i
+                            class="fa-brands fa-instagram"></i></a><a href=""><i class="fa-brands fa-tiktok"></i></a>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 col-ld-4 d-flex align-items-center justify-content-center d-none d-md-flex">
+                <p class="p-rights">#WEAREMADEDELICIOUS</p>
+            </div>
+        </div>
+    </div>
+</footer>
--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -1,38 +1,47 @@
 <header>
-    <div class="upper-banner-header container-fluid ">
-        <div class="row div-upper-header">
-            <div class="col-sd-12 col-md-6 col-ld-6  d-flex justify-content-center align-items-center">
-                <p>Regístrate Ahora: -10% de descuento en tu primer pedido</p>
+    <div class="upper-banner-header container-fluid d-flex justify-content-center">
+        <div class="row div-upper-header container p-1">
+            <div class="col-12 col-md-6 col-ld-6  d-flex justify-content-center align-items-center mt-1 mb-1">
+                <a class="header-promos h-auto" href="">Regístrate Ahora: -10% de descuento en tu primer pedido</a>
             </div>
-            <div class="col-12 col-md-6 col-ld-6 d-flex justify-content-center align-items-center d-none d-md-flex">
-                <p>Todos los Martes: TODAS las hamburguesas al mismo precio</p>
+            <div class="col-12 col-md-6 col-ld-6 d-flex justify-content-center align-items-center">
+                <a class="header-promos h-auto" href="">Todos los Martes: TODAS las hamburguesas al mismo precio</a>
+
             </div>
         </div>
     </div>
-    <nav class="navbar navbar-expand-lg custom-navbar">
-        <div class="container header-container">
-            <img id="header-logo" class="image-fluid" src="../../../public/images/dr_burger_logo.webp" alt="">
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div>
-                    <ul class="navbar-nav ">
-                        <li><a class=" active" aria-current="page" href="#">INICIO</a></li>
-                        <li><a class="nav-link header-link" href="#">MENÚ</a></li>
-                        <li><a class="nav-link header-link" href="#">CONTACTO</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div>
-                <ul class="navbar-nav-right ms-auto d-flex header-icons">
-                    <li><a class="nav-link" href="#"><i class="fa-solid fa-user-large"></i></a></li>
-                    <li><a class="nav-link" href="#"><i class="fa-solid fa-bag-shopping"></i></a></li>
-                    <li><a class="nav-link" href="#"><i class="fa-solid fa-arrow-right-from-bracket"></i></a></li>
-                </ul>
-            </div>
-            <button id="header-toggle" class="navbar-toggler .d-none" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <nav class="container-fluid custom-navbar">
+        <div class=" container header-container p-3 d-flex flex-row align-items-center ">
+            <a href=""><img id="header-logo" class="image-fluid" src="../../../public/images/dr_burger_logo.webp"
+                    alt="logo"></a>
+
+
+            <ul class="d-flex flex-row d-none d-sm-flex mb-0 justify-content-around">
+                <li><a class="active" aria-current="page" href="#">INICIO</a></li>
+                <li><a class="nav-link header-link" href="#">MENÚ</a></li>
+                <li><a class="nav-link header-link" href="#">CONTACTO</a></li>
+            </ul>
+            <ul class="d-flex flex-column d-sm-none mb-0 justify-content-center">
+                <li><a class="active" aria-current="page" href="#">INICIO</a></li>
+                <li><a class="nav-link header-link" href="#">MENÚ</a></li>
+                <li><a class="nav-link header-link" href="#">CONTACTO</a></li>
+            </ul>
+
+
+            <ul class="navbar-nav-right ms-auto d-md-flex header-icons ">
+                <li><a class="nav-link" href="#"><i class="fa-solid fa-user-large fa-lg d-none d-sm-inline"></i><i
+                            class="fa-solid fa-user-large fa-sm d-sm-none"></i></a></li>
+                <li><a class="nav-link" href="#"><i class="fa-solid fa-bag-shopping fa-lg d-none d-sm-inline"></i><i
+                            class="fa-solid fa-bag-shopping fa-sm d-sm-none"></i></a></li>
+                <li><a class="nav-link" href="#"><i
+                            class="fa-solid fa-arrow-right-from-bracket fa-lg d-none d-sm-inline"></i><i
+                            class="fa-solid fa-arrow-right-from-bracket fa-sm d-sm-none"></i></a></li>
+            </ul>
+            </ul>
+
+
+
+
         </div>
     </nav>
 </header>
