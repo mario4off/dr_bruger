5e6f6352774e23faf445487f97032a87a1e813b1
mario4off <mario31991@gmail.com>
2025-01-05 03:57:53 +0100

    cambiar ruta css + favicon

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dr. Burger™ España Oficial</title>
-    <link rel="icon" href="/drburger.com/public/images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="public/images/favicon.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="/drburger.com/public/css/style.css">
+    <link rel="stylesheet" href="public/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&display=swap"
@@ -25,12 +25,9 @@
 include_once('app/views/partials/header.php');
 
 
-
 include_once $view;
 
-// include_once('../partials/body_banner.php');
 
-// echo "<br>";
 
 include_once('app/views/partials/footer.php');
 
--- config/params.php
+++ config/params.php
@@ -3,5 +3,3 @@
 define('url_base', 'http://localhost/drburger.com/');
 
 define('default_action', 'index');
-
-//define('path_base', '/Applications/XAMPP/xamppfiles/htdocs/drburger.com/');
