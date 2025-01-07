c3cf572dbb788f901aebb6c5f8ffd45d37046bd2
mario4off <mario31991@gmail.com>
2025-01-07 11:40:32 +0100

    version DAO herencias de server

--- app/models/BurgerDAO.php
+++ app/models/BurgerDAO.php
@@ -7,7 +7,7 @@ class BurgerDAO
     {
         $con = Database::connect();
 
-        $stmnt = $con->prepare("SELECT * FROM products p JOIN categories c ON p.category_id= c.category_id WHERE category_name LIKE ?");
+        $stmnt = $con->prepare("SELECT p.product_id, p.product_name, p.main_photo, p.category_id, p.base_price FROM products p JOIN categories c ON p.category_id= c.category_id WHERE c.category_name LIKE ?");
         $stmnt->bind_param("s", $filter);
         $stmnt->execute();
         $result = $stmnt->get_result();
